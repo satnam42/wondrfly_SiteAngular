e127c063aa061f088de17ea7cb3fddb3092b6336
Kuldeep <javascript.mspl@gmail.com>
2021-12-03 11:02:06 +0530

    cookies page

---
+++ src/app/core/components/cookies-popup/cookies-popup.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CookiesPopupComponent } from './cookies-popup.component';
+
+describe('AlertComponent', () => {
+  let component: CookiesPopupComponent;
+  let fixture: ComponentFixture<CookiesPopupComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CookiesPopupComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CookiesPopupComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/core/components/cookies-popup/cookies-popup.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cookies-popup',
+  templateUrl: './cookies-popup.component.html',
+  styleUrls: ['./cookies-popup.component.css']
+})
+export class CookiesPopupComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
