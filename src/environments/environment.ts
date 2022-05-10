3baad3451e24837094feedb00b5d4030859eacd1
root <developer.mspl@gmail.com>
2022-05-10 18:58:07 +0530

    latest code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,31 +1,31 @@
-export const environment = {
-  production: false,
-  apiUrls: {
-    reports: 'https://wondrfly.com/api',
-    master: 'https://wondrfly.com/api',
-  },
-  socketUrl: 'https://wondrfly.com:8406',
-  blogsUrl:  'https://cms.wondrfly.com',
-  baseUrl:'https://wondrfly.com/',
-  shareUrl:'https://wondrfly.com/#/',
-
-  name: 'dev'
-
-};
-
-
 // export const environment = {
 //   production: false,
 //   apiUrls: {
-//     reports: 'https://wondrfly.ml/api',
-//     master: 'https://wondrfly.ml/api',
+//     reports: 'https://wondrfly.com/api',
+//     master: 'https://wondrfly.com/api',
 //   },
 //   socketUrl: 'https://wondrfly.com:8406',
-//   blogsUrl:  'https://blogs.wondrfly.ml',
-//   baseUrl:'https://wondrfly.ml/',
-//   shareUrl:'https://wondrfly.ml/#/',
+//   blogsUrl:  'https://cms.wondrfly.com',
+//   baseUrl:'https://wondrfly.com/',
+//   shareUrl:'https://wondrfly.com/#/',
 
 //   name: 'dev'
 
 // };
+
+
+export const environment = {
+  production: false,
+  apiUrls: {
+    reports: 'https://wondrfly.ml/api',
+    master: 'https://wondrfly.ml/api',
+  },
+  socketUrl: 'https://wondrfly.com:8406',
+  blogsUrl:  'https://blogs.wondrfly.ml',
+  baseUrl:'https://wondrfly.ml/',
+  shareUrl:'https://wondrfly.ml/#/',
+
+  name: 'dev'
+
+};
 
